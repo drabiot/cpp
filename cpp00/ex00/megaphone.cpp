003663801a9d0201f2a1a7a0fa9eeb9ca35a178d
drabiot <lipilouc@gmail.com>
2024-07-27 10:49:16 +0200

    Start main

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -9,3 +9,24 @@
 /*   Updated: 2024/07/27 07:48:50 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    std::string string;
+
+    if (argc == 1)
+        std::cout <<  "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (size_t i = 1; argv[i]; i++)
+            string += argv[i];
+        for (size_t j = 1; string[j]; j++)
+            string[j] = std::toupper(string[j]);
+        std::cout << string << std::endl;
+    }
+    return (0);
+}
