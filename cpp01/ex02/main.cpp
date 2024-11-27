561903aeaadbf84cf65a568c472107b56164eafc
Theo Chartier <tchartie@2F1.42angouleme.fr>
2024-11-27 23:02:02 +0100

    Fix ptr

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -20,10 +20,10 @@ int main(void)
 
 
     std::cout << "Memory address of the string variable:  " << &string << std::endl;
-	std::cout << "Memory address held by stringPTR:       " << &stringPTR << std::endl;
+	std::cout << "Memory address held by stringPTR:       " << stringPTR << std::endl;
 	std::cout << "Memory address held by stringREF:       " << &stringREF << std::endl << std::endl;
 
 	std::cout << "Value of the string variable:           " << string << std::endl;
-	std::cout << "Value pointed to by stringPTR:          " << stringPTR << std::endl;
+	std::cout << "Value pointed to by stringPTR:          " << *stringPTR << std::endl;
 	std::cout << "Value pointed to by stringREF:          " << stringREF << std::endl;
 }
