2a8b8d103bd76f5922acc98fd07976461b45f258
Theo Chartier <tchartie@2G2.42angouleme.fr>
2024-12-04 23:48:39 +0100

    Update main

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,14 +6,14 @@
 /*   By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 21:46:30 by tchartie          #+#    #+#             */
-/*   Updated: 2024/12/04 22:32:55 by tchartie         ###   ########.fr       */
+/*   Updated: 2024/12/04 23:48:21 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-static Fixed	scalarProduct(Point const p1, Point const p2, Point const p) {
-	return ((p2.getX() - p1.getX()) * (p.getY() - p1.getY()) - (p2.getY() - p1.getY()) * (p.getX() - p1.getX()));
+static Fixed	scalarProduct(Point const &a, Point const &b, Point const &p) {
+	return (b.getX() - a.getX()) * (p.getY() - a.getY()) - (b.getY() - a.getY()) * (p.getX() - a.getX());
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point ) {
@@ -21,7 +21,7 @@ bool bsp( Point const a, Point const b, Point const c, Point const point ) {
 	Fixed	v2 = scalarProduct(b, c, point);
 	Fixed	v3 = scalarProduct(c, a, point);
 
-	if ((v1 < 0 && v2 < 0 && v3 < 0) || (v1 >= 0 && v2 >= 0 && v3 >= 0))
+	if ((v1 < 0 && v2 < 0 && v3 < 0) || (v1 > 0 && v2 > 0 && v3 > 0))
 		return (true);
 	return (false);
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 22:01:50 by tchartie          #+#    #+#             */
-/*   Updated: 2024/12/04 22:44:45 by tchartie         ###   ########.fr       */
+/*   Updated: 2024/12/04 23:47:10 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,35 @@ int	main( void ) {
 
 		x = 7;
 		y = 3;
+		p = Point(x, y);
+
+		callBsp(a, b, c, p);
+	}
+	std::cout << std::endl << std::endl;
+	{
+		float	x = 0;
+		float	y = 0;
+
+		x = -10;
+		y = -5;
+		Point a(x, y);
+
+		x = 3;
+		y = 1;
+		Point b(x, y);
+		
+		x = -12;
+		y = 6;
+		Point c(x, y);
+
+		x = -6;
+		y = 4;
+		Point p(x, y);
+
+		callBsp(a, b, c, p);
+
+		x = -5;
+		y = 0;
 		p = Point(x, y);
 
 		callBsp(a, b, c, p);
