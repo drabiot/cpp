146b762049bf90755f71e332b2072a55d4baef90
Theo Chartier <tchartie@1G3.42angouleme.fr>
2025-05-01 07:25:00 +0200

    Modify identify

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: tchartie <tchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 15:15:40 by tchartie          #+#    #+#             */
-/*   Updated: 2025/04/17 15:42:00 by tchartie         ###   ########.fr       */
+/*   Updated: 2025/05/01 07:23:10 by tchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,29 +45,26 @@ void	identify(Base* p) {
 }
 
 void	identify(Base& p) {
-	try
-	{
-		A a = dynamic_cast<A&>(p);
+	try {
+		(void)dynamic_cast<A&>(p);
 		PRINT CYAN "Type: " MAGENTA "A" CENDL;
+		return;
 	}
-	catch(const std::exception& e)
-	{
-		try
-		{
-			B b = dynamic_cast<B&>(p);
-			PRINT CYAN "Type: " MAGENTA "B" CENDL;
-		}
-		catch(const std::exception& e)
-		{
-			try
-			{
-				C c = dynamic_cast<C&>(p);
-				PRINT CYAN "Type: " MAGENTA "C" CENDL;
-			}
-			catch (const std::exception &e)
-			{
-				throw (std::out_of_range("Base reference isn't from one of the class."));
-			}
-		}
+	catch (std::exception const &) {}
+
+	try {
+		(void)dynamic_cast<B&>(p);
+		PRINT CYAN "Type: " MAGENTA "B" CENDL;
+		return;
 	}
+	catch (std::exception const &) {}
+
+	try {
+		(void)dynamic_cast<C&>(p);
+		PRINT CYAN "Type: " MAGENTA "C" CENDL;
+		return;
+	}
+	catch (std::exception const &) {}
+
+	throw std::out_of_range("Base reference isn't from one of the known classes.");
 }
